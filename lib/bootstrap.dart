1b8033eccaf3eb263e0b414c1bc0f2c893302a0c
Muhammad Essam <muhammad.essam96@gmail.com>
2024-04-04 19:27:47 +0200

    Implement push notifications on Android using firebase cloud messaging

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tmdb_movies/di.dart';
 import 'package:tmdb_movies/features/movie_details/domain/entities/actor_entity.dart';
@@ -12,6 +14,7 @@ import 'package:tmdb_movies/features/movie_details/domain/entities/credits_entit
 import 'package:tmdb_movies/features/movie_details/domain/entities/genre_entity.dart';
 import 'package:tmdb_movies/features/movie_details/domain/entities/language_entity.dart';
 import 'package:tmdb_movies/features/movie_details/domain/entities/movie_details_entity.dart';
+import 'package:tmdb_movies/features/notifications/services/firebase_cloud_messaging_service.dart';
 import 'package:tmdb_movies/firebase_options.dart';
 
 Future<void> bootstrap(
@@ -45,6 +48,48 @@ Future<void> bootstrap(
       await Hive.openBox<MovieDetails>('movies');
       
       await initializeServiceLocators();
+
+      if (kDebugMode) {
+        final String? fcmToken = await sl<FirebaseCloudMessagingService>().getFcmToken();
+
+        log('FCM token: $fcmToken', name: 'FCM');
+      }
+
+      const AndroidNotificationChannel channel = AndroidNotificationChannel(
+        'high_importance_channel', // id
+        'High Importance Notifications', // title
+        description:  'This channel is used for important notifications.', // description
+        importance: Importance.max,
+      );
+
+      final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+      await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
+
+      await sl<FirebaseCloudMessagingService>().requestPermission();
+
+      sl<FirebaseCloudMessagingService>().listenToForegroundNotifications((RemoteMessage message) {
+        final RemoteNotification? notification = message.notification;
+        final AndroidNotification? android = message.notification?.android; 
+        
+        // If `onMessage` is triggered with a notification, construct our own
+        // local notification to show to users using the created channel.
+        if (notification != null && android != null) {
+          flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ),
+          );
+        }
+      });
 
       runApp(
         await builder(),
--- lib/di.dart
+++ lib/di.dart
@@ -4,12 +4,16 @@ import 'package:get_it/get_it.dart';
 import 'package:tmdb_movies/core/data/remote/dio_client.dart';
 import 'package:tmdb_movies/features/movie_details/di.dart';
 import 'package:tmdb_movies/features/movies/di.dart';
+import 'package:tmdb_movies/features/notifications/services/firebase_cloud_messaging_service.dart';
 
 final GetIt sl = GetIt.instance;
 
 Future<void> initializeServiceLocators() async {
   final Dio dio = initDio(dotenv.env['BASE_URL']!);
-  sl.registerSingleton<Dio>(dio);
+  
+  sl
+    ..registerSingleton<Dio>(dio)
+    ..registerSingleton<FirebaseCloudMessagingService>(FirebaseCloudMessagingService());
 
   initializeMoviesServiceLocators();
 
